7fba922f483d189c21c276ae3f9588969eb937ae
hj5230 <hf305@foxmail.com>
2023-09-03 16:16:49 +0800

    create project

---
+++ src/renderer/src/main.tsx
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom/client'
+import './assets/index.css'
+import App from './App'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <>
+    <App />
+  </>
+)
