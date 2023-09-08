6dce088e5d9004861f558832f7e5fa0602957426
hj5230 <hf305@foxmail.com>
2023-09-09 02:47:16 +0800

    update main panel

--- src/renderer/src/components/MainPanel.tsx
+++ src/renderer/src/components/MainPanel.tsx
@@ -2,25 +2,46 @@ import React from 'react'
 import { Row, Col, Layout } from 'antd'
 import DirHub from './DirHub'
 import FileHub from './FileHub'
+import OperateHub from './OperateHub'
+import style from '../assets/index.module.less'
 
 const { Content } = Layout
 
 interface Props {
   repo: number | null
+  file: number | null
 }
 
-class MainPanel extends React.Component<Props, object> {
+interface State {
+  file: number | null
+}
+
+class MainPanel extends React.Component<Props, State> {
+  state: State = {
+    file: null
+  }
+
+  whichFile = (e: number): void => {
+    this.setState({ file: e })
+  }
+
   render(): React.ReactNode {
+    const { whichFile } = this
     const { repo } = this.props
     return (
       <>
-        <Content style={{ margin: 12 }}>
+        <Content className={style.page_content}>
           <Row gutter={[5, 0]}>
-            <Col span={6}>
-              <DirHub repo={repo} />
+            <Col span={4}>
+              <Row>
+                <OperateHub />
+              </Row>
+              <Row>
+                <DirHub repo={repo} whichFile={whichFile} />
+              </Row>
             </Col>
-            <Col span={18}>
-              <FileHub />
+            <Col span={20}>
+              <FileHub file={null} />
             </Col>
           </Row>
         </Content>
