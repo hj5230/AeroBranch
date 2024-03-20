5f4fe8b776b14521c2945687427ad7d374f6ce2e
hj5230 <hf305@foxmail.com>
2024-03-20 08:12:52 +0200

    add functionality: init local repository

--- src/renderer/src/components/OperateHub.tsx
+++ src/renderer/src/components/OperateHub.tsx
@@ -3,28 +3,47 @@ import Panel from './Panel'
 import Terminal from './Terminal'
 import { Button, Row, Col, Popover } from 'antd'
 import {
-  FullscreenOutlined,
+  FolderAddOutlined,
   BranchesOutlined,
   CodeOutlined,
   SettingOutlined
 } from '@ant-design/icons'
-// import style from '@renderer/assets/index.module.less'
+import Repository from '@renderer/interfaces/Repository'
+// import DotaeroConfig from '@renderer/interfaces/DotaeroConfig'
 
 interface Props {
   windowWidth: number
   windowHeight: number
+  currentRepo: Repository | null
 }
 
 class OperateHub extends React.Component<Props, object> {
+  initRepo = (): void => {
+    const { currentRepo } = this.props
+    const { dotaeroOrInit } = window.api
+    if (currentRepo && currentRepo.localPath) {
+      console.log(currentRepo)
+      dotaeroOrInit(currentRepo.localPath, {
+        userId: -1,
+        macAddress: '',
+        hashedPass: '',
+        createTime: -1,
+        lastModified: -1,
+        structure: currentRepo.files
+      })
+    }
+  }
+
   render(): React.ReactNode {
+    const { initRepo } = this
     const { windowWidth, windowHeight } = this.props
     return (
       <>
         <Panel>
           <Row>
             <Col>
-              <Button size="small">
-                <FullscreenOutlined />
+              <Button size="small" onClick={initRepo}>
+                <FolderAddOutlined />
               </Button>
             </Col>
             <Col>
